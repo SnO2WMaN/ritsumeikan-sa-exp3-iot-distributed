13ea2cfc971e5b0d881e8551fcf634191ed66f8d
SnO2WMaN <me@sno2wman.net>
2022-05-27 19:09:50 +0900

    add edge

---
+++ edge/mod.ts
@@ -0,0 +1,29 @@
+import { connect as amqpConnect } from "amqp";
+
+import { bold, yellow } from "std/fmt/colors";
+
+const edgeId = crypto.randomUUID();
+
+const amqp = await amqpConnect(Deno.env.get("RABBITMQ_URL")!);
+const amqpChan = await amqp.openChannel();
+const amqpQueue = await amqpChan.declareQueue({
+  queue: "pressure",
+  durable: true,
+});
+
+console.log(bold("start"));
+console.log(`edge:${yellow(edgeId)}`);
+
+setInterval(async () => {
+  await amqpChan.publish(
+    { routingKey: amqpQueue.queue },
+    { contentType: "application/json" },
+    new TextEncoder().encode(JSON.stringify(
+      {
+        edge: edgeId,
+        timestamp: Date.now(),
+        value: 100 * Math.random(),
+      },
+    )),
+  );
+}, 1000);
